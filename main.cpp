04a7ce3556e4a904fd87c7bb0e9e2f347392de88
PaBut <pavlo.butenko@thermofisher.com>
2024-11-27 21:58:04 +0100

    added classes declaration

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "simlib.h"
+
+class Visitor : public Process{}
+class VisitorWantsCoffee : public Process{}
+class Consultant : public Process{}
+class Reception : public Process{}
+class ATM : public Process{}
+class CoffeeMachine : public Process{}
+class ATM : public Process{}
+class ComplaintDesk : public Process{}
+
+
+class UserGenerator : public Event{}
+class CashGuysGenerator : public Event{}
+
+
+
+int main(){
+    return 0;
+}
