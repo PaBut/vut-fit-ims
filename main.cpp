cc2f809eb2e811a98539775430f50686d59379fd
PaBut <pavlo.butenko@thermofisher.com>
2024-11-30 02:22:18 +0100

    added user generator and user service processing

--- main.cpp
+++ main.cpp
@@ -1,18 +1,53 @@
 #include "simlib.h"
 
-class Visitor : public Process{}
-class VisitorWantsCoffee : public Process{}
-class Consultant : public Process{}
-class Reception : public Process{}
-class ATM : public Process{}
-class CoffeeMachine : public Process{}
-class ATM : public Process{}
-class ComplaintDesk : public Process{}
+Queue Reception();
+Queue ComplaintDesk();
+Store Consultants(5);
+Store CoffeeMachines(3);
+Store ATMs(4);
 
 
-class UserGenerator : public Event{}
-class CashGuysGenerator : public Event{}
+class Visitor : public Process{
+    public:
+        Visitor();
+        void Behavior() override;
+};
+class VisitorWantsCoffee : public Process{};
+class Consultant : public Process{};
+class Reception : public Process{};
+class ATM : public Process{};
+class CoffeeMachine : public Process{};
+class ATM : public Process{};
+class ComplaintDesk : public Process{};
 
+
+class UserGenerator : public Event{
+    public:
+        UserGenerator(double expTime){
+            this->ExpTime = expTime;
+        }
+    private:
+        double ExpTime;
+    void Behavior(){
+        (new Visitor())->Activate();
+        this->Activate(Time + Exponential(ExpTime));
+    }
+};
+class CashGuysGenerator : public Event{};
+
+
+void Visitor::Behavior(){
+    auto serviceType = Random();
+    if(serviceType < 0.2){
+        // Reception processing
+    }
+    else if(serviceType >= 0.2 && serviceType < 0.7){
+        // ATM
+    }
+    else{
+        // Consultants
+    }
+};
 
 
 int main(){
