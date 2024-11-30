301730443292eca735b0b2ec99abb42220a7b0aa
PaBut <pavlo.butenko@thermofisher.com>
2024-11-30 03:03:57 +0100

    added reception services processing

--- main.cpp
+++ main.cpp
@@ -1,55 +1,117 @@
 #include "simlib.h"
 
-Queue Reception();
-Queue ComplaintDesk();
+#define minutes(min) min * 60
+
+Facility Reception("reception");
+Queue ReceptionQueue("reception");
+Facility ComplaintDesk();
 Store Consultants(5);
 Store CoffeeMachines(3);
 Store ATMs(4);
 
+class Visitor : public Process
+{
+public:
+    Visitor();
+    void Behavior() override;
+    void setGoForCoffee();
 
-class Visitor : public Process{
-    public:
-        Visitor();
-        void Behavior() override;
+private:
+    bool goesForCoffee;
+    void WentForCoffee();
 };
-class VisitorWantsCoffee : public Process{};
-class Consultant : public Process{};
-class Reception : public Process{};
-class ATM : public Process{};
-class CoffeeMachine : public Process{};
-class ATM : public Process{};
-class ComplaintDesk : public Process{};
 
+class VisitorWantsCoffee : public Process
+{
+private:
+    Visitor *visitor;
+    int maxAwaitTime;
 
-class UserGenerator : public Event{
-    public:
-        UserGenerator(double expTime){
-            this->ExpTime = expTime;
-        }
-    private:
-        double ExpTime;
-    void Behavior(){
+public:
+    VisitorWantsCoffee(Visitor *visitor, int maxAwaitTime)
+    {
+        this->visitor = visitor;
+        this->maxAwaitTime = maxAwaitTime;
+    }
+
+    void Behavior() override
+    {
+        visitor->Passivate();
+        Wait(maxAwaitTime);
+        visitor->Activate();
+        visitor->setGoForCoffee();
+    }
+};
+
+class ATM : public Process
+{
+};
+
+class VisitorGenerator : public Event
+{
+public:
+    VisitorGenerator(double expTime)
+    {
+        this->ExpTime = expTime;
+    }
+
+private:
+    double ExpTime;
+    void Behavior()
+    {
         (new Visitor())->Activate();
         this->Activate(Time + Exponential(ExpTime));
     }
 };
-class CashGuysGenerator : public Event{};
+class CashGuysGenerator : public Event
+{
+};
 
-
-void Visitor::Behavior(){
+void Visitor::Behavior()
+{
     auto serviceType = Random();
-    if(serviceType < 0.2){
+    if (serviceType < 0.2)
+    {
         // Reception processing
+        while (Reception.Busy())
+        {
+            Into(ReceptionQueue);
+            auto wantsCoffee = new VisitorWantsCoffee(this, minutes(8));
+            wantsCoffee->Activate();
+
+            if (goesForCoffee)
+            {
+                Out();
+                WentForCoffee();
+            }
+        }
+
+        Seize(Reception);
+        Wait(Normal(minutes(5), minutes(2)));
+        Release(Reception);
+        if (!ReceptionQueue.Empty())
+        {
+            ReceptionQueue.GetFirst()->Activate();
+        }
     }
-    else if(serviceType >= 0.2 && serviceType < 0.7){
+    else if (serviceType >= 0.2 && serviceType < 0.7)
+    {
         // ATM
     }
-    else{
+    else
+    {
         // Consultants
     }
 };
 
+void Visitor::WentForCoffee()
+{
+    Enter(CoffeeMachines);
+    Wait(minutes(1));
+    Leave(CoffeeMachines);
+}
 
-int main(){
+int main()
+{
     return 0;
 }
